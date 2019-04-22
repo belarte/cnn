1280080b08e3cbfd3d2a889076fd657da549e5ee
Bruno Belarte <bruno.belarte@gmail.com>
2019-04-22 11:34:01 +0000

    Change ASSERT_EQ to use == instead of !=

--- matrices/test.cpp
+++ matrices/test.cpp
@@ -11,7 +11,7 @@
 
 #define ASSERT_EQ(left, right) \
    do { \
-      if (left != right) { \
+      if (!(left == right)) { \
          std::cerr << "Error in file " << __FILE__ << ":" << __LINE__; \
 			std::cerr << " in function " << __func__ << "() => " << #left << " != " << #right; \
 			std::cerr << "\nwith " << #left << " =\n" << left; \
