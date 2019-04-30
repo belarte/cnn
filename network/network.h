6ca3cb80666a10ea3c00debd8b09e84bf0021a00
Bruno Belarte <bruno.belarte@gmail.com>
2019-04-30 08:21:39 +0000

    Add weights layers to network

--- network/network.h
+++ network/network.h
@@ -2,14 +2,26 @@
 
 #include "matrices/matrices.h"
 
-#include <array>
 #include <tuple>
+
+template<size_t A, size_t B, size_t... Args>
+struct Weights
+{
+	using type = decltype(std::tuple_cat(std::tuple<Matrix<A, B>>{}, typename Weights<B, Args...>::type{}));
+};
+
+template<size_t A, size_t B>
+struct Weights<A, B>
+{
+	using type = std::tuple<Matrix<A, B>>;
+};
 
 template<size_t... Args>
 class Network
 {
 public:
 	using NeuronLayers = std::tuple<Matrix<1, Args, double>...>;
+	using WeightLayers = typename Weights<Args...>::type;
 
 	constexpr Network()
 	{
@@ -17,4 +29,5 @@ public:
 
 private:
 	NeuronLayers m_neuronLayers;
+	WeightLayers m_weightLayers;
 };
--- network/test.cpp
+++ network/test.cpp
@@ -4,7 +4,7 @@ int main(int, char**)
 {
 	std::cout << "Start testing..." << std::endl;
 
-	Network<2, 3> n;
+	Network<2, 5, 3> n;
 
 	std::cout << "Done!" << std::endl;
 	return 0;
