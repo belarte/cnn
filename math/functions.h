e1786c145e4f56e4b16608c199d25da52fcca397
Bruno Belarte <bruno.belarte@gmail.com>
2019-06-26 20:02:06 +0000

    Add logistic function definition

--- math/functions.h
+++ math/functions.h
@@ -1,5 +1,7 @@
 #pragma once
 
+#include <cmath>
+
 namespace function {
 
 struct Identity
@@ -10,6 +12,17 @@ struct Identity
 
 	static constexpr double fp(double) {
 		return 1;
+	}
+};
+
+struct Logistic
+{
+	static double f(double x) {
+		return 1.0 / (1 + std::exp(-x));
+	}
+
+	static double fp(double x) {
+		return f(x) * (1 - f(x));
 	}
 };
 
