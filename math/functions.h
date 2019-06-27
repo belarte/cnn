d35a97c037b4a25f155d37908ade378bccc24c02
Bruno Belarte <bruno.belarte@gmail.com>
2019-06-27 20:23:55 +0000

    Cosmetic

--- math/functions.h
+++ math/functions.h
@@ -6,22 +6,26 @@ namespace function {
 
 struct Identity
 {
-	static constexpr double f(double x) {
+	static constexpr double f(double x)
+	{
 		return x;
 	}
 
-	static constexpr double fp(double) {
+	static constexpr double fp(double)
+	{
 		return 1;
 	}
 };
 
 struct Logistic
 {
-	static double f(double x) {
+	static double f(double x)
+	{
 		return 1.0 / (1 + std::exp(-x));
 	}
 
-	static double fp(double x) {
+	static double fp(double x)
+	{
 		return f(x) * (1 - f(x));
 	}
 };
